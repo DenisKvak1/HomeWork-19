3ecea20ef6b180e2f8c24d5cc19f6fd13d8d9e65
DenisKvak1 <denis.kvak457@gmail.com>
2023-11-11 19:49:12 +0200

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,7 @@ let Button2 = document.getElementById("calcA");
 let InputMASS= document.getElementById("mass");
 let InputHeight = document.getElementById("height");
 let result2 = document.getElementById("resultIMT");
+let result3 = document.getElementById("resultIMT2");
 
 Button.addEventListener("click", function() {
     if(!isNaN(LeapYear(Input.value))){
@@ -38,24 +39,31 @@ Button.addEventListener("click", function() {
 });
 Button2.addEventListener("click", function() {
     if(InputMASS.value && InputHeight.value){
-        let imt=IMT(InputMASS.value,InputHeight.value/100)
-        if(!isNaN(imt)){
-            if (imt < 18.5) {
-                result2.textContent = "Недостаточная масса тела (дефицит массы).";
-            } else if (imt >= 18.5 && imt <= 24.9) {
-                result2.textContent = "Нормальная масса тела.";
-            } else if (imt >= 25 && imt <= 29.9) {
-                result2.textContent = "Избыточная масса тела (предожирение).";
-            } else if (imt >= 30 && imt <= 34.9) {
-                result2.textContent = "Ожирение I степени.";
-            } else if (imt >= 35 && imt <= 39.9) {
-                result2.textContent = "Ожирение II степени.";
-            } else if (imt > 40) {
-                result2.textContent = "Ожирение III степени (крайне высокий уровень ожирения).";
-            }            
-        }
+        let imt=IMT(InputMASS.value,InputHeight.value)
+        if(InputHeight.value<2.6){
+            if(!isNaN(imt)){
+                result3.textContent=`Ваш имт: ${Math.round(imt)}`;
+                if (imt < 18.5) {
+                    result2.textContent = "Недостаточная масса тела (дефицит массы).";
+                } else if (imt >= 18.5 && imt <= 24.9) {
+                    result2.textContent = "Нормальная масса тела.";
+                } else if (imt >= 25 && imt <= 29.9) {
+                    result2.textContent = "Избыточная масса тела (предожирение).";
+                } else if (imt >= 30 && imt <= 34.9) {
+                    result2.textContent = "Ожирение I степени.";
+                } else if (imt >= 35 && imt <= 39.9) {
+                    result2.textContent = "Ожирение II степени.";
+                } else if (imt > 40) {
+                    result2.textContent = "Ожирение III степени (крайне высокий уровень ожирения).";
+                }            
+            }
+            else{
+                result2.textContent="Введите корректные данные";
+            }
+        } 
         else{
-            result2.textContent="Введите корректные данные";
+            result3.textContent="";
+            result2.textContent="Введите данные в метрах xD";
         }
     }});
 
